3bfafc25eb0af102ca5fe52fa0e635d9378674d9
wahyunf354 <wahyunurfadillah313@gmail.com>
2021-01-06 10:05:35 +0700

    fix error navbar mobile

--- layouts/sidebar.php
+++ layouts/sidebar.php
@@ -6,10 +6,10 @@
 
     <!-- Sidebar - Brand -->
     <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
-      <div class="sidebar-brand-icon rotate-n-15">
-        <i class="fas fa-laugh-wink"></i>
+      <div class="sidebar-brand-icon">
+        <i class="fas fa-store"></i>
       </div>
-      <div class="sidebar-brand-text mx-3">SB Admin <sup>2</sup></div>
+      <div class="sidebar-brand-text mx-3">Baca Buku</div>
     </a>
 
     <!-- Divider -->
@@ -17,7 +17,7 @@
 
     <!-- Nav Item - Dashboard -->
     <li class="nav-item active">
-      <a class="nav-link" href="index.html">
+      <a class="nav-link" href="/toko-buku-uas">
         <i class="fas fa-fw fa-tachometer-alt"></i>
         <span>Dashboard</span></a>
     </li>
@@ -35,16 +35,16 @@
 
     <!-- Nav Item - Charts -->
     <li class="nav-item">
-      <a class="nav-link" href="charts.html">
-        <i class="fas fa-fw fa-chart-area"></i>
-        <span>Charts</span></a>
+      <a class="nav-link" href="/toko-buku-uas/buku">
+        <i class="fas fa-fw fa-book"></i>
+        <span>Buku</span></a>
     </li>
 
     <!-- Nav Item - Tables -->
     <li class="nav-item">
-      <a class="nav-link" href="tables.html">
-        <i class="fas fa-fw fa-table"></i>
-        <span>Tables</span></a>
+      <a class="nav-link" href="/toko-buku-uas/kategori">
+        <i class="fas fa-fw fa-list"></i>
+        <span>Kategori</span></a>
     </li>
 
     <!-- Divider -->
