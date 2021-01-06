c1e336ec3e822c1a30107404ffc78457191d46b1
wahyunf354 <wahyunurfadillah313@gmail.com>
2021-01-06 09:41:20 +0700

    init project

---
+++ layouts/footer.php
@@ -0,0 +1,20 @@
+<!-- Bootstrap core JavaScript-->
+<script src="/toko-buku-uas/assets/vendor/jquery/jquery.min.js"></script>
+<script src="/toko-buku-uas/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+<!-- Core plugin JavaScript-->
+<script src="/toko-buku-uas/assets/vendor/jquery-easing/jquery.easing.min.js"></script>
+
+<!-- Custom scripts for all pages-->
+<script src="/toko-buku-uas/assets/js/sb-admin-2.min.js"></script>
+
+<!-- Page level plugins -->
+<script src="/toko-buku-uas/assets/vendor/chart.js/Chart.min.js"></script>
+
+<!-- Page level custom scripts -->
+<script src="/toko-buku-uas/assets/js/demo/chart-area-demo.js"></script>
+<script src="/toko-buku-uas/assets/js/demo/chart-pie-demo.js"></script>
+
+</body>
+
+</html>
